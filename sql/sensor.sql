fd66260a02722d5d0a719642cb1e03821c6de61e
Terris Linenbach <terrisgit@terris.com>
2016-05-03 11:04:50 -0700

    Working for the first time

--- sql/sensor.sql
+++ sql/sensor.sql
@@ -1,7 +1,13 @@
-CREATE KEYSPACE sensor WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
+CREATE KEYSPACE sensor with 
+durable_writes=true 
+and REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+
 USE sensor;
 
-CREATE TABLE sensor (partner_id TEXT, name TEXT, id TEXT, location TEXT, site TEXT, type TEXT, properties map<TEXT,TEXT>, row_id timeuuid PRIMARY KEY (partner_id,site,location,id));
+CREATE TABLE sensor (partner_id TEXT, name TEXT, id TEXT, location TEXT, 
+    site TEXT, type TEXT, properties map<TEXT,TEXT>, row_id timeuuid, PRIMARY KEY (partner_id,site,location,id));
 
-CREATE TABLE measurement_by_date (sensor_id TEXT, measured_date date, measured timestamp, measure TEXT, text_value TEXT, double_value double, row_id timeuuid PRIMARY KEY ((sensor_id, date),timestamp,measure)) WITH CLUSTERING ORDER BY (timestamp DESC, measure ASC);
+CREATE TABLE measurement_date (sensor_id TEXT, measured_date date, measured timestamp, measure TEXT, 
+    text_value TEXT, double_value double, row_id timeuuid, PRIMARY KEY ((sensor_id, measured_date),measured,measure)) 
+WITH CLUSTERING ORDER BY (measured DESC, measure ASC);
 
