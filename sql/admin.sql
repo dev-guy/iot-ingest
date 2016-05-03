1fce4b7db363e61d9f7e5b42a856b2e06f44a48e
Terris Linenbach <terrisgit@terris.com>
2016-05-03 10:56:21 -0700

    Working for the first time

--- sql/admin.sql
+++ sql/admin.sql
@@ -1,9 +1,14 @@
-CREATE KEYSPACE admin WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
+CREATE KEYSPACE admin with 
+durable_writes=true 
+and REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+
 USE admin;
 
-CREATE TABLE partner (id TEXT, name TEXT, properties map<TEXT,TEXT>, hashed_api_key TEXT, hashed_api_key_salt row_id timeuuid PRIMARY KEY (id) );
+CREATE TABLE partner (id TEXT, name TEXT, properties map<TEXT,TEXT>, 
+hashed_api_key TEXT, hashed_api_key_salt text, 
+row_id timeuuid, PRIMARY KEY (id));
 
-CREATE TABLE mqtt_server ( id timeuuid, host TEXT, port int, max_clients int PRIMARY KEY (host, port) );
+CREATE TABLE mqtt_server ( id timeuuid, host TEXT, port int, max_clients int, PRIMARY KEY (host, port) );
 
-CREATE TABLE mqtt_server_client ( mqtt_server_id timeuuid, clients set<int>, row_id timeuuid PRIMARY KEY (mqtt_server_id));
+CREATE TABLE mqtt_server_client ( mqtt_server_id timeuuid, clients set<int>, row_id timeuuid, PRIMARY KEY (mqtt_server_id));
 
