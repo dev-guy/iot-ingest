5d3353b988e64a21fed1baaa15d510c0fbea409e
Terris Linenbach <terrisgit@terris.com>
2016-05-04 21:03:13 -0700

    Whatever

---
+++ hash.js
@@ -0,0 +1,20 @@
+const CryptoJS = require("crypto-js");
+
+function hashIt(message, hash) {
+  var hash = CryptoJS.SHA256(message.concat(hash))
+  return hash.toString();
+}
+
+function randomStr(m) {
+  if (m < 9) {
+    m = 9;
+  }
+  var s = '', r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz_-!@#$%^()~|[]{}\"\'\\';
+  for (var i=0; i < m; i++) { s += r.charAt(Math.floor(Math.random()*r.length)); }
+  return s;
+};
+
+function randomSalt() {
+  return randomStr(Math.floor(Math.random()*50) )
+}
+
