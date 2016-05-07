258e44098a8c7f9985c39768bca6a2726386faa1
Terris Linenbach <terrisgit@terris.com>
2016-05-07 14:16:23 -0700

    Add user table

--- sql/admin.sql
+++ sql/admin.sql
@@ -4,8 +4,13 @@ and REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
 
 USE admin;
 
-CREATE TABLE partner (id TEXT, name TEXT, properties map<TEXT,TEXT>, 
-hashed_api_key TEXT, hashed_api_key_salt text, 
+CREATE TABLE partner (id TEXT, name TEXT, properties map<TEXT,TEXT>,
+encrypted_api_key TEXT, encrypted_api_key_salt_len int,
+hashed_password TEXT, hashed_password_salt text,
 row_id timeuuid, PRIMARY KEY (id));
 
+CREATE TABLE user (partner_id TEXT, id TEXT, name TEXT, properties map<TEXT,TEXT>,
+email_address TEXT,
+hashed_password TEXT, hashed_password_salt text,
+row_id timeuuid, PRIMARY KEY (id));
 
