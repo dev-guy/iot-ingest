90deced7b4a513cd381fcd0465db155e39d2d399
Terris Linenbach <terrisgit@terris.com>
2016-05-15 16:14:40 -0700

    x

--- sql/admin.sql
+++ sql/admin.sql
@@ -4,13 +4,15 @@ and REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
 
 USE admin;
 
-CREATE TABLE partner (id TEXT, name TEXT, properties map<TEXT,TEXT>,
+CREATE TABLE partner (id TEXT, name TEXT,
 encrypted_api_key TEXT, encrypted_api_key_salt_len int,
-hashed_password TEXT, hashed_password_salt text,
-row_id timeuuid, PRIMARY KEY (id));
-
-CREATE TABLE user (partner_id TEXT, id TEXT, name TEXT, properties map<TEXT,TEXT>,
 email_address TEXT,
+PRIMARY KEY (id));
+
+CREATE TABLE user (
+partner_id TEXT,
+email_address TEXT,
+name TEXT,
 hashed_password TEXT, hashed_password_salt text,
-row_id timeuuid, PRIMARY KEY (id));
+PRIMARY KEY (partner_id, email_address));
 
